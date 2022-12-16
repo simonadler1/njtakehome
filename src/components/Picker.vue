45d20f64a80470dd3ee4e1cf6feab1adbd3ca981
simon <adler.shimon@gmail.com>
2022-12-16 11:11:31 -0600

    added overlow auto y to view details

---
+++ src/components/Picker.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="overflow-y-auto" height="50vh">
+    <v-list>
+      <v-list-item-group v-model="selectedPokemon" color="primary">
+        <v-list-item v-for="(item, idx) in getPokemon.results" :key="idx">
+          <v-list-item-content @click="selectPokemon(item)">
+            <v-list-item-title v-text="item.name"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item> </v-list-item-group></v-list
+  ></v-card>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      selectedPokemon: undefined,
+    };
+  },
+  computed: {
+    ...mapGetters(["getPokemon"]),
+  },
+  methods: {
+    ...mapActions(["selectPokemon"]),
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/ViewDetails.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card height="50vh" class="overflow-y-auto">
+    <div v-if="!getSelectedPokemon">Select A Pokemon To Get Started!</div>
+    <div v-else>
+      <v-img
+        width="200"
+        height="175"
+        :src="getSelectedPokemon.sprites.front_default"
+      ></v-img>
+      <v-card-title> Name:{{ getSelectedPokemon.name }}</v-card-title>
+      <v-card-title> Height: {{ getSelectedPokemon.height }} </v-card-title>
+      <v-card-title> Weight: {{ getSelectedPokemon.weight }} </v-card-title>
+      <v-card-text v-if="getSelectedPokemon.location"
+        >Location: {{ getSelectedPokemon.location }}</v-card-text
+      >
+    </div>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["getSelectedPokemon"]),
+  },
+};
+</script>
+
+<style></style>
